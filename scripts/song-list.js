9d2f841ae570c33b33256781df80b617022fdcec
Caroline <carolfalabella@gmail.com>
2019-09-21 15:00:43 -0700

    Add songs list

---
+++ scripts/song-list.js
@@ -0,0 +1,30 @@
+{
+   // iterate through album.songs, and for each song object,
+   // create a new song element, and then append that element to the #song-list table.
+   album.songs.forEach( (song, index) => {
+     song.element = $(`
+       <tr>
+         <td>
+           <button>
+             <span class="song-number">${index + 1}</span>
+             <span class="ion-play"></span>
+             <span class="ion-pause"></span>
+           </button>
+         </td>
+         <td>${song.title}</td>
+         <td>${song.duration}</td>
+        </tr>
+     `);
+     // play/pause song element
+     song.element.on('click', event => {
+       player.playPause(song);
+     });
+     // hover over show/hide elements
+     song.element.on
+
+
+
+
+    $('#song-list').append(song.element);
+   });
+}
